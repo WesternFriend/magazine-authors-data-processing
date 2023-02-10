cb48d908561ab46d067350a8137abd7c38a6679e
Brylie Christopher Oxley <brylie@amble.fi>
2023-02-10 18:35:01 +0200

    Add magazine authors name parser

---
+++ magazine_authors_name_parser.py
@@ -0,0 +1,38 @@
+"""
+Given a raw Drupal export of Magazine Authors,
+parse the author name into given and family name columns.
+"""
+
+import csv
+
+parsed_authors = []
+
+with open("magazine_authors_uncleaned.csv") as authors_csv:
+    reader = csv.reader(authors_csv)
+    authors = list(reader)
+
+    for author in authors:
+        original_name = author[0]
+        author_split = original_name.split(sep=" ")
+        family_name = author_split.pop()
+        given_name = " ".join(author_split)
+
+        parsed_author = {
+            "given_name": given_name,
+            "family_name": family_name,
+            "original_name": original_name,
+        }
+
+        parsed_authors.append(parsed_author)
+
+with open("magazine_authors_parsed.csv", "w", newline="") as parsed_authors_csv:
+    fieldnames = ["given_name", "family_name", "original_name"]
+    writer = csv.DictWriter(
+        parsed_authors_csv,
+        fieldnames=fieldnames,
+    )
+
+    writer.writeheader()
+
+    for author in parsed_authors:
+        writer.writerow(author)
